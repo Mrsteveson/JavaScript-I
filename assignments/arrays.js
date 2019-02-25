8d170f1c44ae7ba701fa4e6596ffccd409af6410
mrsteveson <patrick.steveson727@gmail.com>
2019-02-25 17:14:49 -0600

    finished array number6

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -112,10 +112,16 @@ console.log(oldCars);
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
+// I am going to need an if statement and a true/false boolean situation
+
 let BMWAndAudi = [];
-for (let i = 0; i < inventory.length; i++)
+for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].car_make == "BMW" || inventory[i].car_make == "Audi") {
+        BMWAndAudi.push(inventory[i])
+    }
+}
 
-
+console.log(JSON.stringify(BMWAndAudi));
 console.log(BMWAndAudi);
 
 
